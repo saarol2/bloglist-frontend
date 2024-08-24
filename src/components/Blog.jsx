39828bd8aa7cd10cc83ae127a2c3720400f08f19
Oliver Saari <saarioliver02@gmail.com>
2024-08-24 15:31:59 +0300

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({ blog }) => {
+
+  const [visibility, setVisibility] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisibility(!visibility)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>
+          {visibility ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visibility && (
+        <div>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          {blog.user.name}
+        </div>
+      )}
+    </div>
+  )
+}
 export default Blog
